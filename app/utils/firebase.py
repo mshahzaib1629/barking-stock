eb2fea3b0bb251eb63a59a8b67e3936ab5002538
Shahzaib Minhas <mshahzaib1629@gmail.com>
2024-08-31 03:44:48 +0500

    path redefined in firebase

--- app/utils/firebase.py
+++ app/utils/firebase.py
@@ -1,9 +1,23 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import os
+
+def _find_project_root(current_path, marker_files=('.git', 'setup.py', 'requirements.txt')):
+    # Keep traversing up until we find a known project file or directory
+    while current_path != os.path.dirname(current_path):  # While we haven't reached the top
+        if any(os.path.exists(os.path.join(current_path, marker)) for marker in marker_files):
+            return current_path
+        current_path = os.path.dirname(current_path)
+    
+    return None
 
 def initialize_firebase():
-    cred = credentials.Certificate('./firebase_service_account.json')
+    current_file_path = os.path.dirname(os.path.abspath(__file__))
+    project_root_path = _find_project_root(current_file_path)
+    firebase_creds_path = os.path.join(project_root_path, 'firebase_service_account.json')
+
+    cred = credentials.Certificate(firebase_creds_path)
     app = firebase_admin.initialize_app(cred)
     db = firestore.client()
     print("Firebase Initialized!")
